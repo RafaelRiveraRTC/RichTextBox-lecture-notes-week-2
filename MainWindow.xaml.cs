573200799073b09ef5ff543896674b50a061ee11
RafaelRiveraRTC <Tropicalcone25@gmail.com>
2024-04-10 19:12:31 -0700

    Update Completed the notes and tested the code snippets in the window console

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,5 +21,74 @@ namespace RichTextBox_lecture_notes_week_2
             InitializeComponent();
             //main window
         }
+        //This is a demonstration of a summary
+
+        //summary
+        /// Test method to 
+        /// summary
+        /// <param name="number1">Number to pass in</param>
+        /// <param name="word1">word to pass in</param>
+        /// 
+
+        public string TestMethod(int number1, string word1)
+        {
+            return "";
+        }
+
+        private void Info_Display(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //Steps
+                //1.Create string variables for first and last name
+                string firstName = First.Text;
+                string lastName = Last.Text;
+                
+                //2. Concatinate them together to form a full name
+                //This is an example of string interpolation
+                string fullName = $"{firstName} {lastName}";
+                //3. Display that information to our label
+                LbL.Content = fullName;
+
+                //4.Display to rich textbox
+                runDisplay.Text = fullName;
+
+                // Add a new Rich Text Box for getting an address
+                // Add a label for the Rich Text box
+                // Properly name the "Run" tag
+                // Write the code to grab the text from RTB
+                string address = runHomeInfo.Text;
+                // Display it to our original runDisplay
+
+                //two line breaks
+                runDisplay.Text += "/n/n";
+
+                //Append the address
+                runDisplay.Text += address;
+
+                //Full Name
+
+
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+          
+
+        }
+
+        private void RichTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void RichTextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }//class
+
+
 }//program
--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # RichTextBox lecture notes week 2
+
+Richtext box: a box that accepts text, will accept paragrpaphs
+and larger text
+
+ //Steps
+            //1.Create string variables for first and last name
+            string firstName = First.Text;
+            string lastName = Last.Text;
+            //2. Concatinate them together to form a full name
+            //This is an example of string interpolation
+            string fullName = $"{firstName} {lastName}";
+            //3. Display that information to our label
+            LbL.Content = fullName;
+
+
+double click the button to make a click event
+
+ex is the error object, .message only displays message,to string will display the full info behind the error
+
+make sure the button,textbox and label names match in the .cs file
+
+int number = int.Parse(firstName)/int.Parse(lastName);
+
+catch(DividdeByZeroException dvz) messageBox.Show(You cannot divide by zero);
+
+Flow documents sit inside your rich text box,they are like a peice of paper
+
+<paragrpah> Paragraph of text
+
+<run> is a single sentence
+
+Flow document is an object with a list of paragraphs,with a list of runs. 
+
+You don't name the rich text box you name the run
+
+Adding a new rich text box for typing your address
+
+Add a label for the rich text box
+
+Properly name the run tag
+
+ Write the code to grab the text from RTB
+ string address = runHomeInfo
+
+  Display it to our original runDisplay
+
+  Any time you can change the text, use.Text,otherwise use .label
